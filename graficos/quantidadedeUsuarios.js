2b8384c565d20b70d765a8f9ac6e52ce5685b266
lebecasa <148072673+lebecasa@users.noreply.github.com>
2024-11-14 08:40:15 -0300

    Update quantidadedeUsuarios.js

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,1 +1,22 @@
+async function quantidadeUsuariosPorRedes() {
+  const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const nomeDasRedes = Object.keys()
+  const quantidadeDeUsuarios = Object.values(dados)
+ 
+  const data = [
+  {
+    x: nomeDasRedes,
+    y: quantidadeDeUsuarios,
+    type: 'bar'
+  }
+]
 
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementBlyld('graficos-container').appendChild(grafico)
+  Ploty.newPlot(grafico, data)
+}
+
+  quantidadeUsuariosPorRedes()
